ef0fefd91fcab1de5f6bb4588384f8ed926c36b7
Sebastian Stuecker <sebastian.stuecker@googlemail.com>
2016-12-10 23:31:08 +0100

    added snapcast module

---
+++ 96_Snapcast.pm
@@ -0,0 +1,289 @@
+package main;
+use strict;
+use warnings;
+use Switch;
+use Data::Dumper;
+use JSON::Parse 'parse_json';
+
+my $Snapcast_LogLevelDebug = 1;
+my $Snapcast_LogLevelNormal = 1;
+my $Snapcast_LogLevelCritical =1;
+
+
+my %Snapcast_gets = (
+	"tbd"	=> "x"
+);
+
+my %Snapcast_sets = (
+    "tbd"   => "x"
+);
+
+sub Snapcast_Initialize($) {
+    my ($hash) = @_;
+	require "$attr{global}{modpath}/FHEM/DevIo.pm";
+    $hash->{DefFn}      = 'Snapcast_Define';
+    $hash->{UndefFn}    = 'Snapcast_Undef';
+    $hash->{SetFn}      = 'Snapcast_Set';
+    $hash->{GetFn}      = 'Snapcast_Get';
+	$hash->{WriteFn}    = 'Snapcast_Write';
+	$hash->{ReadyFn}    = 'Snapcast_Ready';
+    #$hash->{AttrFn}     = 'Snapcast_Attr';
+    $hash->{ReadFn}     = 'Snapcast_Read';
+    $hash->{TIMEOUT}	= 0.1;
+    $hash->{AttrList} =
+          "startcmd stopcmd interval "
+        . $readingFnAttributes;
+	
+
+}
+
+sub Snapcast_Define($$) {
+    my ($hash, $def) = @_;
+    my @a = split('[ \t]+', $def);
+    $hash->{name}  = $a[0];
+    $hash->{ip} = (defined($a[2])) ? $a[2] : "localhost"; 
+    $hash->{port} = (defined($a[3])) ? $a[3] : "1705"; 
+    readingsSingleUpdate($hash,"state","defined",1);
+    RemoveInternalTimer($hash);
+    DevIo_CloseDev($hash);
+    $hash->{DeviceName} = $hash->{ip}.":".$hash->{port};
+    DevIo_OpenDev(
+            $hash, 0,
+            "Snapcast_OnConnect",
+        );
+    return undef;
+}
+
+sub Snapcast_Undef($$) {
+    my ($hash, $arg) = @_; 
+    RemoveInternalTimer($hash);
+    return undef;
+}
+
+
+sub Snapcast_Get($@) {
+	my ($hash, @param) = @_;
+	return '"get Snapcast" needs at least one argument' if (int(@param) < 2);
+	
+	my $name = shift @param;
+	my $opt = shift @param;
+	if(!$Snapcast_gets{$opt}) {
+		my @cList = keys %Snapcast_gets;
+		return "Unknown argument $opt, choose one of " . join(" ", @cList);
+	}
+	return "to be defined";
+}
+
+sub Snapcast_Set($@) {
+	my ($hash, @param) = @_;
+	
+	return '"set Snapcast" needs at least one argument' if (int(@param) < 2);
+	
+	my $name = shift @param;
+	my $opt = shift @param;
+	my $value = join("", @param);
+	
+	if(!defined($Snapcast_sets{$opt})) {
+		my @cList = keys %Snapcast_sets;
+		return "Unknown argument $opt, choose one of " . join(" ", @cList);
+	}
+	return "not yet implemented";
+}
+
+sub Snapcast_Read($)
+{
+  my ($hash) = @_;
+  my $name = $hash->{NAME};
+  my $buf;
+  
+  $buf = DevIo_SimpleRead($hash);
+    return "" if ( !defined($buf) );
+  $buf = $hash->{PARTIAL} . $buf;
+  
+  my $lastchr = substr( $buf, -1, 1 );
+  if ( $lastchr ne "\n" ) {
+      $hash->{PARTIAL} = $buf;
+      Log3( $hash, 5, "snap: partial command received" );
+      return;
+  }
+  else {
+      $hash->{PARTIAL} = "";
+  }
+
+  my @lines = split( "\n", $buf );
+  foreach my $line (@lines) {
+    # Hier die Results parsen
+    Log3 $name, 3, $line;
+    my $update=parse_json($line);
+    if($update->{method}=~/Client/){
+    	my $c=$update->{params}->{data};
+    	Snapcast_UpdateClient($hash,$c,0);
+    }
+    if($update->{method}=~/Stream/){
+    	my $s=$update->{params}->{data};
+    	Snapcast_UpdateStream($hash,$s,0);
+    }
+
+  }
+}
+
+
+sub Snapcast_Ready($)
+{
+  my ($hash) = @_;
+  my $name = $hash->{NAME};
+  if (AttrVal($hash->{NAME}, 'disable', 0)) {
+    return;
+  }
+  if ( ReadingsVal( $name, "state", "disconnected" ) eq "disconnected" ) {
+  		fhem "deletereading ".$name." .*";
+        DevIo_OpenDev($hash, 1,"Snapcast_OnConnect");
+        return;
+    }
+
+  return undef;
+}
+
+sub Snapcast_OnConnect($)
+{
+  my ($hash) = @_;
+  my $name = $hash->{NAME};
+  $hash->{LAST_CONNECT} = FmtDateTime( gettimeofday() );
+  $hash->{CONNECTS}++;
+  $hash->{helper}{PARTIAL} = "";
+  Snapcast_GetStatus($hash);
+  return undef;
+ }
+
+sub Snapcast_UpdateClient($$$){
+	my ($hash,$c,$cnumber) = @_;
+	if($cnumber==0){
+		$cnumber++;
+		while(defined($hash->{STATUS}->{clients}->{"$cnumber"}) && $c->{host}->{mac} ne $hash->{STATUS}->{clients}->{"$cnumber"}->{host}->{mac}){$cnumber++}
+		if (not defined ($hash->{STATUS}->{clients}->{"$cnumber"})) { return undef;}
+	}
+	$hash->{STATUS}->{clients}->{"$cnumber"}=$c;
+ 	readingsBeginUpdate($hash);
+ 	readingsBulkUpdateIfChanged($hash,"clients_".$cnumber."_online",$c->{connected} ? 'true' : 'false' );
+ 	readingsBulkUpdateIfChanged($hash,"clients_".$cnumber."_name",$c->{config}->{name} ? $c->{config}->{name} : $c->{host}->{name} );
+  	readingsBulkUpdateIfChanged($hash,"clients_".$cnumber."_latency",$c->{config}->{latency} );
+  	readingsBulkUpdateIfChanged($hash,"clients_".$cnumber."_stream",$c->{config}->{stream} );
+  	readingsBulkUpdateIfChanged($hash,"clients_".$cnumber."_volume",$c->{config}->{volume}->{percent} );
+  	readingsBulkUpdateIfChanged($hash,"clients_".$cnumber."_muted",$c->{config}->{volume}->{muted} ? 'true' : 'false' );
+  	readingsBulkUpdateIfChanged($hash,"clients_".$cnumber."_ip",$c->{host}->{ip} );
+  	readingsBulkUpdateIfChanged($hash,"clients_".$cnumber."_mac",$c->{host}->{mac});
+  	readingsEndUpdate($hash,1);
+}
+
+sub Snapcast_UpdateStream($$$){
+	my ($hash,$s,$snumber) = @_;
+	if($snumber==0){
+		$snumber++;
+		while(defined($hash->{STATUS}->{streams}->{"$snumber"}) && $s->{id} ne $hash->{STATUS}->{streams}->{"$snumber"}->{id}){$snumber++}
+		if (not defined ($hash->{STATUS}->{streams}->{"$snumber"})){ return undef;}
+	}
+	$hash->{STATUS}->{streams}->{"$snumber"}=$s;
+ 	readingsBeginUpdate($hash);	
+ 	readingsBulkUpdateIfChanged($hash,"streams_".$snumber."_id",$s->{id} );
+	readingsBulkUpdateIfChanged($hash,"streams_".$snumber."_status",$s->{status} );
+  	readingsEndUpdate($hash,1);
+}
+
+sub Snapcast_GetStatus($){
+  my ($hash) = @_;
+  my $name = $hash->{NAME};
+  my $request;
+  my $line = DevIo_Expect( $hash,Snapcast_Encode($hash,'Server.GetStatus'),1);
+  my $status=parse_json($line);
+  my $streams=$status->{result}->{streams};
+  my $clients=$status->{result}->{clients};
+  my $server=$status->{result}->{server};
+
+  
+  $hash->{STATUS}->{server}=$server;
+  if(defined ($clients)){
+  	my @clients=@{$clients};
+  	my $cnumber=1;
+  	foreach my $c(@clients){
+	  	Snapcast_UpdateClient($hash,$c,$cnumber);
+	  	$cnumber++;
+  	}
+  }
+  if(defined ($streams)){
+  	my @streams=@{$streams} unless not defined ($streams);
+  	my $snumber=1;
+  	foreach my $s(@streams){
+	  	Snapcast_UpdateStream($hash,$s,$snumber);
+	  	$snumber++;
+  	}
+  }
+  
+  #Log3 $name, 3, Dumper($clients[0]);
+  InternalTimer(gettimeofday() + 600, "Snapcast_GetStatus", $hash, 1);
+}
+
+
+
+sub Snapcast_Encode($$){
+  my ($hash,$method) = @_;
+  my $name = $hash->{NAME};
+  if(defined($hash->{helper}{REQID})){$hash->{helper}{REQID}++;}else{$hash->{helper}{REQID}=1;}
+  my $request;
+  $request->{jsonrpc}="2.0";
+  $request->{method}=$method;
+  $request->{id}=$hash->{helper}{REQID};
+  Log3 $name,3,encode_json($request)."\r\n";
+  return encode_json($request)."\r\n";
+}
+
+
+
+1;
+
+=pod
+=begin html
+
+<a name="Snapcast"></a>
+<h3>Snapcast</h3>
+<ul>
+    <i>PASeerver</i> is the server side module for the FHEM Pulseaudio Multiroom Solution. It automates the process of managing
+	the dynamic creation and deletion of Pulseaudio Tunnel and Combine Sinks in order to create a truly synced multiroom audiio experience
+	Pulseaudio capable of transfering audio to any other computer running Pulseaudio, and also to more than one at once. Snapcast integrates this capability
+	into FHEM and combined with the PAClient module it enables the FHEM user to have several clients listen to different or the same source of audio in sync or independently on demand
+	The Snapcast is only useful if there is at least one PAClient while the multiroom feature is only useable with at least 2 clients obviously.
+	Direct playback on the server is not yet implemented
+    <br><br>
+    <a name="Snapcastdefine"></a>
+    <b>Define</b>
+    <ul>
+        <code>define <name> Snapcast <ip></code>
+        <br><br>
+        Example: <code>define MyServer Snapcast 127.0.0.1</code>
+        <br><br>
+        The ip parameter gives the hostname or ip of the machine where pulseaudio is running on and where the audio source is going to be played
+		Currently the Snapcast and PAClient framework is work in progress. It is planned to directly integrate with the mpd sound server using the FHEM MPD module.
+        See <a href="http://fhem.de/commandref.html#define">commandref#define</a> 
+        for more info about the define command.
+    </ul>
+    <br>
+    
+    <a name="Snapcastset"></a>
+    <b>Set</b><br>
+    <ul>
+        <code>set <name> <option> <value></code>
+        <br><br>
+        See <a href="http://fhem.de/commandref.html#set">commandref#set</a> 
+        for more info about the set command.
+        <br><br>
+        Options:
+        <ul>
+              <li><i>tbd</i><br>
+                  tbd</li>
+        </ul>
+    </ul>
+    <br>
+</ul>
+
+=end html
+
+=cut
